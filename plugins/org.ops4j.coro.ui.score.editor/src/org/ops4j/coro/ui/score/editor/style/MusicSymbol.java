ce641908429be12c4b1f0a3a7757dd91b931a7c0
Harald Wellmann <harald.wellmann@gmx.de>
2013-07-06 11:57:27 +0200

    new symbols flags, rests and dot

--- plugins/org.ops4j.coro.ui.score.editor/src/org/ops4j/coro/ui/score/editor/style/MusicSymbol.java
+++ plugins/org.ops4j.coro.ui.score.editor/src/org/ops4j/coro/ui/score/editor/style/MusicSymbol.java
@@ -6,7 +6,21 @@ public enum MusicSymbol {
     
     NOTEHEAD_WHOLE('\ue169'),
     NOTEHEAD_HALF('\ue13f'),
-    NOTEHEAD_QUARTER('\ue13e');
+    NOTEHEAD_QUARTER('\ue13e'),
+    DOT('\ue17c'),
+
+    FLAGS_8TH_UP('\ue180'),
+    FLAGS_16TH_UP('\ue182'),
+    FLAGS_8TH_DOWN('\ue17f'),
+    FLAGS_16TH_DOWN('\ue181'),
+    
+    REST_WHOLE('\ue166'),
+    REST_HALF('\ue162'),
+    REST_QUARTER('\ue15d'),
+    REST_EIGHTH('\ue164'),
+    REST_16TH('\ue165'),
+    
+    ;
 
     private char character;
     
